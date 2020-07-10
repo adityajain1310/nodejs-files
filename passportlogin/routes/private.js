615bf0d3c3dd7774e905ca548fc89ed7bac0d3ec
Aditya Jain <jain13102000@gmail.com>
2020-07-10 14:31:16 +0530

    login with passport.authentication
    Signed-off-by: Aditya Jain <jain13102000@gmail.com>

--- passportlogin/routes/private.js
+++ passportlogin/routes/private.js
@@ -1,7 +1,12 @@
 const route = require('express').Router()
 
 route.get('/', (req, res) => {
-    res.send("visible to only logged in users")
+    if (req.user) {
+        res.send("visible to only logged in users")
+    } else {
+        res.redirect('/login')
+    }
+    
 })
 
 exports = module.exports = route
